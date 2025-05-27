da9d94240e6493ffd0ed8f999c4f4815439cbf21
VedPanse <panse.ved@yandex.com>
2025-05-27 10:05:47 -0700

    added draggable class

---
+++ Pomodoro/DraggableHostingView.swift
@@ -0,0 +1,21 @@
+import Cocoa
+import SwiftUI
+
+class DraggableHostingView<Content>: NSHostingView<Content> where Content: View {
+    private var initialLocation: NSPoint = .zero
+
+    override func mouseDown(with event: NSEvent) {
+        guard let window = self.window else { return }
+        initialLocation = NSEvent.mouseLocation
+        initialLocation.x -= window.frame.origin.x
+        initialLocation.y -= window.frame.origin.y
+    }
+
+    override func mouseDragged(with event: NSEvent) {
+        guard let window = self.window else { return }
+        let currentLocation = NSEvent.mouseLocation
+        var newOrigin = NSPoint(x: currentLocation.x - initialLocation.x,
+                                y: currentLocation.y - initialLocation.y)
+        window.setFrameOrigin(newOrigin)
+    }
+}
