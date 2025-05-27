d54d7291de9029ae2b1e02ec04a5d9d9cc254f9d
VedPanse <panse.ved@yandex.com>
2025-05-27 09:29:04 -0700

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Ved Panse on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
